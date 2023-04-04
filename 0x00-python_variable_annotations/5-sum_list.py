b29977cd7894fa38dba9cd6fb4d533b23a69ba55
Kwabena Sapong <kwabenasapong@gmail.com>
2023-04-04 09:02:37 +0000

    Update task 6

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -4,8 +4,9 @@
 """
 
 
-def sum_list(input_list: list[float]) -> float:
-    sum: float = 0
-    for x in input_list:
-        sum += x
-    return sum
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """returns their sum as a float."""
+    return sum(input_list)
