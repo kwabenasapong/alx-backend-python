8f7cae28d3faf33e9189ca1145c0be5bdb72bdce
Kwabena Sapong <kwabenasapong@gmail.com>
2023-04-04 07:59:54 +0000

    Add task 1

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""0. Basic annotations - add
+"""
+
+
+def add(x: float, y: float) -> float:
+    """Add Function
+
+    Args:
+        x (float): [description]
+        y (float): [description]
+
+    Returns:
+        float: [description]
+    """
+    return (x + y)
