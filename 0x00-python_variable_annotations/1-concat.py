a702fccb1eb675a2c014f987083956ac4bb31067
Kwabena Sapong <kwabenasapong@gmail.com>
2023-04-04 08:09:53 +0000

    Add task 2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""1. Basic annotations - concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concat Function
+
+    Args:
+        str1 (str): [description]
+        str2 (str): [description]
+
+    Returns:
+        str: [description]
+    """
+    return (f"{str1}{str2}")
