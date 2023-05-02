f9424329bcb5ad51ca85920cf063fea1895c300f
Kwabena Sapong <kwabenasapong@gmail.com>
2023-05-02 09:29:55 +0000

    Solve task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+'''
+Take the code from wait_n and alter it into a new
+function task_wait_n. The code is nearly identical
+to wait_n except task_wait_random is being called.
+'''
+
+import asyncio
+import random
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    '''Take the code from wait_n and alter it into a new function'''
+    list_of_delays = []
+    for i in range(n):
+        list_of_delays.append(await task_wait_random(max_delay))
+    return sorted(list_of_delays)
