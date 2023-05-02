7568640b5fb38209e6fbb73b6d8bc658c5366867
Kwabena Sapong <kwabenasapong@gmail.com>
2023-05-02 08:44:58 +0000

    Update task 2

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -28,8 +28,3 @@ def measure_time(n: int, max_delay: int) -> float:
     end_time = time.perf_counter()
     total_time = end_time - start_time
     return total_time / n
-
-n = 5
-max_delay = 9
-
-print(measure_time(n, max_delay))
